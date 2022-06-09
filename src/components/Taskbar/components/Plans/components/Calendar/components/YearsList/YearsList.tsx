e676918bd06fb535d6c5137c0042378bc4730f31
damian08k <damiankierzkowski08@gmail.com>
2022-06-09 23:09:33 +0200

    Switch view to months if user click year in years view

--- src/components/Taskbar/components/Plans/components/Calendar/components/YearsList/YearsList.tsx
+++ src/components/Taskbar/components/Plans/components/Calendar/components/YearsList/YearsList.tsx
@@ -1,6 +1,9 @@
 import { FC } from 'react';
 
 import useFillYears from 'hooks/useFillYears';
+import { calendarActions } from 'store/slices/calendar.slice';
+import { currentDateActions } from 'store/slices/currentDate.slice';
+import { useAppDispatch } from 'store/store';
 import { YearType } from 'types/components/calendar/yearType.type';
 import getSplittedToday from 'utils/getSplittedToday';
 
@@ -11,9 +14,17 @@ type Props = {
 };
 
 const YearsList: FC<Props> = ({ today }) => {
-  const { year } = getSplittedToday(today);
+  const dispatch = useAppDispatch();
 
-  const years = useFillYears(year);
+  const { year: currentYear } = getSplittedToday(today);
+
+  const years = useFillYears(currentYear);
+
+  const handleYearClick = (year: number) => {
+    dispatch(currentDateActions.updateMonthAndYear({ month: null, year }));
+    dispatch(calendarActions.setIsYearsView(false));
+    dispatch(calendarActions.setIsMonthsView(true));
+  };
 
   return (
     <div className={classes.root}>
@@ -23,6 +34,7 @@ const YearsList: FC<Props> = ({ today }) => {
           className={`${classes.yearElement} ${type === YearType.CURRENT && classes.current} ${
             classes[type.toLowerCase()]
           }`}
+          onClick={() => handleYearClick(year)}
         >
           {year}
         </div>
