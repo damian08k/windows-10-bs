1c3901c26efdd4b01c2fdc5274d6850d6b1203f6
damian08k <damiankierzkowski08@gmail.com>
2022-06-12 00:37:12 +0200

    asdasd

--- src/components/Taskbar/components/TimeAndDate/components/CurrentTime/CurrentTime.tsx
+++ src/components/Taskbar/components/TimeAndDate/components/CurrentTime/CurrentTime.tsx
@@ -7,7 +7,7 @@ import classes from './CurrentTime.module.css';
 
 const CurrentTime: FC = () => {
   const time = useSelector((state: RootState) => state.clock.time);
-  return <div className={classes.root}>{`${time?.substring(0, 5)}`}</div>;
+  return <div className={classes.root}>{`${time?.substring('1', 5)}`}</div>;
 };
 
 export default CurrentTime;
