93b28465561f3fe5e5788401b5e23c89d9f75e03
damian08k <damiankierzkowski08@gmail.com>
2022-08-26 22:42:09 +0200

    Display selected day in Events component

--- src/components/Taskbar/components/Plans/components/Events/Events.tsx
+++ src/components/Taskbar/components/Plans/components/Events/Events.tsx
@@ -1,11 +1,24 @@
 import { FC } from 'react';
+import { useSelector } from 'react-redux';
+
+import { RootState } from 'types/store/store.type';
+
+import { TODAY_ID } from 'src/constants';
+
+import changeSelectedDateToDayName from 'utils/changeSelectedDateToDayName';
 
 import classes from './Events.module.css';
 
 const Events: FC = () => {
+  const { selectedDay } = useSelector((state: RootState) => state.calendar);
+
+  const selectedDayName = changeSelectedDateToDayName(selectedDay).toLowerCase();
+
   return (
     <div className={classes.root}>
-      <div className={classes.date}>today...</div>
+      <div className={classes.date}>
+        {selectedDay.id === TODAY_ID ? 'Today' : `${selectedDayName} ${selectedDay.selectedDay}`}
+      </div>
       <input className={classes.input} placeholder="Add an event or reminder" />
       <div className={classes.events}>events...</div>
     </div>
