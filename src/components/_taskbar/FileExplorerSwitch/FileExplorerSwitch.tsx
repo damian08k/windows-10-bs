cdd94a303bf966122906dfa58b8807eeb09e64c4
damian08k <damiankierzkowski08@gmail.com>
2023-04-24 23:56:46 +0200

    feat: Add file explorer icon to the menu bar

---
+++ src/components/_taskbar/FileExplorerSwitch/FileExplorerSwitch.tsx
@@ -0,0 +1,13 @@
+import { ReactComponent as FileExplorerIcon } from 'assets/icons/file-explorer.svg';
+
+import classes from './FileExplorerSwitch.module.css';
+
+export const FileExplorerSwitch = () => {
+  return (
+    <div className={classes.root}>
+      <button className={classes.explorerButton} aria-label="Open file explorer">
+        <FileExplorerIcon className={classes.fileExplorerIcon} />
+      </button>
+    </div>
+  );
+};
