cf0d6e616fe94278e89152ef0ec01048db7be90c
damian08k <damiankierzkowski08@gmail.com>
2023-04-27 23:28:46 +0200

    feat: Add possibility to close file explorer by clicking closing icon

--- src/components/_explorer/ExplorerTopBar/components/ActionsBar/ActionsBar.tsx
+++ src/components/_explorer/ExplorerTopBar/components/ActionsBar/ActionsBar.tsx
@@ -1,3 +1,7 @@
+import { useDispatch } from 'react-redux';
+
+import { explorerActions } from 'store/slices/explorer.slice';
+
 import { mergeClasses } from 'utils/mergeClasses';
 
 import { ReactComponent as CloseIcon } from 'assets/icons/close.svg';
@@ -5,6 +9,12 @@ import { ReactComponent as CloseIcon } from 'assets/icons/close.svg';
 import classes from './ActionsBar.module.css';
 
 export const ActionsBar = () => {
+  const dispatch = useDispatch();
+
+  const handleCloseWindow = () => {
+    dispatch(explorerActions.toggleExplorerVisibility(false));
+  };
+
   return (
     <div className={classes.root}>
       <button
@@ -18,6 +28,7 @@ export const ActionsBar = () => {
       <button
         className={mergeClasses(classes.actionButton, classes.close)}
         aria-label="Close window"
+        onClick={handleCloseWindow}
       >
         <CloseIcon className={classes.closeIcon} />
       </button>
